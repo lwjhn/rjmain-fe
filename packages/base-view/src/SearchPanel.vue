3980724c59841c10da36978cdd1a12ffaf74e1cf
zhuanglw <lwjhn0118@163.com>
2021-10-09 19:34:58 +0800

    -

--- packages/base-view/src/SearchPanel.vue
+++ packages/base-view/src/SearchPanel.vue
@@ -1,8 +1,9 @@
 <template>
-    <el-form-item v-bind="config">
-        <el-select v-if="/select/i.test(config.type)" v-model="config.value" filterable
-                   :multiple="config.multiple"
-                   placeholder="请选择">
+    <el-form-item v-bind="Object.assign({}, config, {
+        width: undefined, type: undefined , value: undefined, criteria: undefined
+    })">
+        <el-select v-if="/select/i.test(config.type)" v-model="config.value"
+                   v-bind="Object.assign({filterable: true}, config.bind)">
             <el-option
                 v-for="(item, pos) in config.options"
                 :key="pos" v-if="item"
@@ -11,7 +12,7 @@
             </el-option>
         </el-select>
         <el-checkbox-group v-else-if="/checkbox/i.test(config.type)"
-                           v-model="config.value">
+                           v-model="config.value" v-bind="config.bind">
             <el-checkbox
                 v-for="(item, pos) in config.options"
                 :key="pos" v-if="item"
@@ -19,14 +20,14 @@
             </el-checkbox>
         </el-checkbox-group>
         <el-radio-group v-else-if="/radiobutton/i.test(config.type)"
-                        v-model="config.value">
+                        v-model="config.value" v-bind="config.bind">
             <el-radio-button
                 v-for="(item, pos) in config.options"
                 :key="pos" v-if="item"
                 :label="item.value!==undefined ? item.value : item.label">{{ item.label }}
             </el-radio-button>
         </el-radio-group>
-        <el-radio-group v-else-if="/radio/i.test(config.type)">
+        <el-radio-group v-else-if="/radio/i.test(config.type)" v-bind="config.bind">
             <el-radio
                 v-for="(item, pos) in config.options"
                 :key="pos" v-if="item"
@@ -35,18 +36,20 @@
         </el-radio-group>
         <el-input-number v-else-if="/number/i.test(config.type)"
                          v-model="config.value" controls-position="right"
-                         :step="typeof config.step === 'number' ? config.step : 1"
-                         :min="config.min ? config.min : 0"
-                         :max="config.max ? config.max : 999999999999999"
+                         v-bind="Object.assign({
+                            step: 1, min:0, max: 999999999999999
+                        }, config.bind)"
         ></el-input-number>
         <el-date-picker v-else-if="/date/i.test(config.type)"
                         v-model="config.value"
-                        type="daterange"
-                        align="right"
-                        unlink-panels
-                        range-separator="至"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期"
+                        v-bind="Object.assign({
+                            type:'daterange',
+                            align:'right',
+                            unlinkPanels: true,
+                            rangeSeparator:'至',
+                            startPlaceholder:'开始日期',
+                            endPlaceholder:'结束日期'
+                        }, config.bind)"
         >
         </el-date-picker>
         <el-input v-else v-model="config.value" @input="$forceUpdate()" clearable
